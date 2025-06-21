6fdadbdcc4d2440e01755036c60851497ceb7405
iamsanju123 <sk6191368@gmail.com>
2025-06-21 15:19:06 +0530

    add new cors

--- src/app.ts
+++ src/app.ts
@@ -9,13 +9,21 @@ dotenv.config({
 const app: express.Application = express();
 
 app.use(cookieParser(process.env.COOKIE_SECURE));
+
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  cors({
-    // origin: "http://localhost:5173",
-    // credentials: true,
-  })
-);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // or '*' (less secure)
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  
+  // Handle preflight
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204); // No Content
+  }
+
+  next();
+});
 app.use(express.json());
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
